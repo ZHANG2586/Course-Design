390a9728be689cdca4bda0f3d388626fc1605448
ZHANG2586 <66770812+ZHANG2586@users.noreply.github.com>
2020-06-12 16:22:11 +0800

    Create Play_UI.h

---
+++ View/Play_UI.h
@@ -0,0 +1,27 @@
+/*Description : ������Ŀҳ���*/
+
+
+#ifndef PLAY_UI_H_
+#define PLAY_UI_H_
+
+//��ʶ����TTMS_SCU_PLAY_UI_MgtEnt
+//���ܣ�������Ŀ����
+void Play_UI_MgtEntry(void);
+
+//��ʶ����TTMS_SCU_PLAY_UI_Add
+//���ܣ������¾�Ŀ����
+int Play_UI_Add(void);
+
+//��ʶ����TTMS_SCU_PLAY_UI_Mod
+//���ܣ��޸��¾�Ŀ����
+int Play_UI_Modify(int id);
+
+//��ʶ����TTMS_SCU_PLAY_UI_Mod
+//���ܣ��޸��¾�Ŀ����
+int Play_UI_Delete(int id);
+
+//��ʶ����TTMS_SCU_PLAY_UI_Qry
+//���ܣ���ѯ��Ŀ����
+int Play_UI_Query(int id);
+
+#endif
